aa63321c9341cf699bcf5def2cc00c1e71fa4958
Norman Breau <norman@normanbreau.com>
2020-05-05 23:07:02 -0300

    tests

--- spec/WAPImage.spec.ts
+++ spec/WAPImage.spec.ts
@@ -4,9 +4,12 @@
 import {
     WAPTestTools,
     WAPReader,
-    WebAssetPack,
-    PluginManager
+    // WebAssetPack,
+    PluginManager,
+    WAPTestFactory,
+    TestWAP
 } from '@wap/test-tools';
+import {IDictionary} from '@totalpave/interfaces';
 import Blob from 'node-blob';
 import WAPImage from '../src/WAPImage';
 import def from './support/def.json';
@@ -16,8 +19,8 @@ import def from './support/def.json';
 
 
 describe('WAPImage', () => {
-    let reader: WAPReader = new WAPReader();
-    let pack: WebAssetPack = null;
+    let reader: WAPReader = new WAPReader(new WAPTestFactory());
+    let pack: TestWAP = null;
 
     beforeAll(async () => {
         (<any>global).Blob = Blob;
@@ -26,7 +29,7 @@ describe('WAPImage', () => {
         PluginManager.getInstance().addPlugin('gif', new WAPImage());
         PluginManager.getInstance().addPlugin('png', new WAPImage());
         let buffer: ArrayBuffer = await WAPTestTools.compile(def);
-        pack = await reader.read(buffer);
+        pack = <TestWAP>(await reader.read(buffer));
     });
 
     beforeEach(() => {
@@ -34,17 +37,29 @@ describe('WAPImage', () => {
     });
 
     const files: Array<string> = [
-        'dice.png',
-        'earth.gif',
-        'img.jpeg',
-        'img.jpg'
+        'dice.png'
+        // 'earth.gif',
+        // 'img.jpeg',
+        // 'img.jpg'
     ];
 
+    let checksums: IDictionary<string> = {
+        'dice.png': '32351df8f7942ad9bacd1e5cf55f8ed2',
+        'earth.gif': '0f992d2987bd3151057eedc85220ecb6',
+        'img.jpeg': 'fe87d1319c2c2bd2d5c41b113f4aa070',
+        'img.jpg': 'fe87d1319c2c2bd2d5c41b113f4aa070'
+    };
+
     files.forEach((file: string) => {
-        it(file, async () => {
+        it(`File Check: ${file}`, async () => {
             await pack.get(file);
             expect(URL.createObjectURL).toHaveBeenCalled();
             expect((<jasmine.Spy>URL.createObjectURL).calls.mostRecent().args[0]).toBeInstanceOf(Blob);
+        });
+
+        it(`MD5 Checksum Check: ${file}`, async () => {
+            console.warn(pack.hexdump(file).slice(0, 100));
+            expect(pack.md5(file)).toBe(checksums[file]);
         });
     });
 });
